166a28c2f340cf9829e006382383275b65799735
Daryl Hedley <darylhedley@hotmail.com>
2014-01-02 10:59:32 +0000

    Pushed develop into master
    Squashed commit of the following:
    
    commit 47d360a33798c3e186e6ac37a7677fbd5751e9ea
    Author: Daryl Hedley <darylhedley@hotmail.com>
    Date:   Thu Jan 2 10:17:20 2014 +0000
    
        Updated .bowerrc file
    
    commit e99f6f4aab91d68742605629ac7a624622324fd1
    Author: Daryl Hedley <darylhedley@hotmail.com>
    Date:   Mon Dec 16 22:35:44 2013 +0000
    
        Updated menu styling, templates and JS

---
+++ js/adapt-contrib-boxmenu.js
@@ -0,0 +1,80 @@
+define(function(require) {
+
+    var Backbone = require('backbone');
+    var Adapt = require('coreJS/adapt');
+    var MenuView = require('coreViews/menuView');
+    
+    var BoxMenuView = MenuView.extend({
+
+        preRender: function() {
+            this.listenTo(this.model, 'change:_isReady', this.isReady);
+            $('.loading').fadeIn('fast');
+            this.$el.hide();
+        },
+        
+        postRender: function() {
+            var nthChild = 0;
+            this.model.getChildren().each(function(item) {
+                if(item.get('_isAvailable')) {
+                    nthChild ++;
+                    this.$('.menu-inner').append(new BoxMenuItemView({model:item, nthChild:nthChild}).$el);
+                }
+            });
+        },
+        
+        isReady: function() {
+            _.defer(_.bind(function() {
+                $('.loading').hide();
+                this.$el.fadeIn('slow');
+                Adapt.trigger('menuView:ready');
+            }, this));
+        }
+
+    }, {
+        template:'boxmenu'
+    });
+
+    var BoxMenuItemView = MenuView.extend({
+
+        className: function() {
+            return 'menu-item ' 
+            + 'menu-item-' 
+            + this.model.get('_id') 
+            + " nth-child-" 
+            + this.options.nthChild;;
+        },
+
+        events: {
+            'click a':'clickItem'
+        },
+
+        clickItem: function() {
+            console.log('clicked');
+            this.model.set('_isVisited', true);
+        },
+
+        preRender: function() {
+
+        },
+
+        postRender: function() {
+            this.$el.imageready(_.bind(function() {
+                this.setReadyStatus();
+            }, this));
+        }
+
+    }, {
+        template:'boxmenu-item'
+    });
+    
+    Adapt.on('router:menu', function(model) {
+        console.log(model);
+        $('#wrapper').append(new BoxMenuView({model:model}).$el);
+    });
+    
+    /*Adapt.on('router:menu', function(id) {
+        var currentModel = Adapt.contentObjects.findWhere({_id:id});
+        new BoxMenuView({model:currentModel});
+    });*/
+    
+});
