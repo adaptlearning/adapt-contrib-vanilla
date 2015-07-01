80953439f3efb8bd1202faf0fb21a2d71788408d
kirsty-hames <kirstyhames@kirsty.local>
2015-07-01 17:34:45 +0100

    ABU-755 hide assessment graphics for mobile view

--- js/vanilla.js
+++ js/vanilla.js
@@ -4,6 +4,18 @@ define(function(require) {
 	var Backbone = require('backbone');
 	var ThemeBlock = require('theme/adapt-contrib-vanilla/js/theme-block');
 
+
+	// Page View
+	// =========
+
+	Adapt.on('pageView:postRender', function(view) {
+		var pageClasses = view.model.get('_classes');
+
+		if (pageClasses) {
+			this.$('.page').addClass(pageClasses);
+		}
+	});
+
 	// Block View
 	// ==========
 
