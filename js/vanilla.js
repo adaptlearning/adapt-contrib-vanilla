f23eaee1050bcabd713e4ff1d1f8a6c080a86dd3
Thomas Berger <thomas.berger@learnchamp.com>
2015-07-31 11:51:13 +0200

    removed redundant code

--- js/vanilla.js
+++ js/vanilla.js
@@ -4,18 +4,6 @@ define(function(require) {
 	var Backbone = require('backbone');
 	var ThemeBlock = require('theme/adapt-contrib-vanilla/js/theme-block');
 
-
-	// Page View
-	// =========
-
-	Adapt.on('pageView:postRender', function(view) {
-		var pageClasses = view.model.get('_classes');
-
-		if (pageClasses) {
-			this.$('.page').addClass(pageClasses);
-		}
-	});
-
 	// Block View
 	// ==========
 
