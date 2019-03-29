0869c73c707bb9e583a706523efa32c08b525445
Nils Sanderson <nils@learningpool.com>
2019-03-29 14:43:30 +0000

    update to use 2 spaces for indentation

--- js/theme-block.js
+++ js/theme-block.js
@@ -1,59 +1,59 @@
 define(function(require) {
-	
-	var Adapt = require('coreJS/adapt');
-	var Backbone = require('backbone');
+  
+  var Adapt = require('coreJS/adapt');
+  var Backbone = require('backbone');
 
-	var ThemeBlockView = Backbone.View.extend({
+  var ThemeBlockView = Backbone.View.extend({
 
-		initialize: function() {
-			this.setStyles();
-			this.listenTo(Adapt, 'device:resize', this.setStyles);
-			this.listenTo(Adapt, 'remove', this.remove);
-		},
+    initialize: function() {
+      this.setStyles();
+      this.listenTo(Adapt, 'device:resize', this.setStyles);
+      this.listenTo(Adapt, 'remove', this.remove);
+    },
 
-		setStyles: function() {
-			this.setBackground();
-			this.setMinHeight();
-			this.setDividerBlock();
-		},
+    setStyles: function() {
+      this.setBackground();
+      this.setMinHeight();
+      this.setDividerBlock();
+    },
 
-		setBackground: function() {
-			var backgroundColor = this.model.get('_themeBlockConfig')._backgroundColor;
-			
-			if (backgroundColor) {
-				this.$el.addClass(backgroundColor);
-			}
-		},
+    setBackground: function() {
+      var backgroundColor = this.model.get('_themeBlockConfig')._backgroundColor;
 
-		setMinHeight: function() {
-			var minHeight = 0;
-			var minHeights = this.model.get('_themeBlockConfig')._minimumHeights;
+      if (backgroundColor) {
+        this.$el.addClass(backgroundColor);
+      }
+    },
 
-			if (minHeights) {
+    setMinHeight: function() {
+      var minHeight = 0;
+      var minHeights = this.model.get('_themeBlockConfig')._minimumHeights;
 
-				if(Adapt.device.screenSize == 'large') {
-					minHeight = minHeights._large;
-				} else if (Adapt.device.screenSize == 'medium') {
-					minHeight = minHeights._medium;
-				} else {
-					minHeight = minHeights._small;
-				}
-			}
+      if (minHeights) {
 
-			this.$el.css({
-				minHeight: minHeight + "px"
-			});
-		},
+        if(Adapt.device.screenSize == 'large') {
+          minHeight = minHeights._large;
+        } else if (Adapt.device.screenSize == 'medium') {
+          minHeight = minHeights._medium;
+        } else {
+          minHeight = minHeights._small;
+        }
+      }
 
-		setDividerBlock: function() {
-			var dividerBlock = this.model.get('_themeBlockConfig')._isDividerBlock;
+      this.$el.css({
+        minHeight: minHeight + "px"
+      });
+    },
 
-			if (dividerBlock) {
-				this.$el.addClass('divider-block');
-			}
-		}
-	});
+    setDividerBlock: function() {
+      var dividerBlock = this.model.get('_themeBlockConfig')._isDividerBlock;
 
-	return ThemeBlockView;
-	
+      if (dividerBlock) {
+        this.$el.addClass('divider-block');
+      }
+    }
+  });
+
+  return ThemeBlockView;
+  
 });
