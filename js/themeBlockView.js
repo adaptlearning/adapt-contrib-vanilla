bc596e81267a1aa100c2173266779e15134563d7
Guy Willis <guy.willis@kineo.com>
2019-08-22 14:08:25 +0100

    Updated block divider class name

--- js/themeBlockView.js
+++ js/themeBlockView.js
@@ -6,7 +6,7 @@ define([
   var ThemeBlockView = ThemeView.extend({
 
     className: function() {
-      return this.model.get("_isDividerBlock") ? "divider-block" : "";
+      return this.model.get("_isDividerBlock") ? "is-divider-block" : "";
     },
 
     setCustomStyles: function() {},
