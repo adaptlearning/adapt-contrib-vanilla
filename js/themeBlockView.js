91b48537e8eb25c98b39714a41e317c66b5d1b3f
Guy Willis <guy.willis@kineo.com>
2022-11-03 16:37:27 +0000

    component vertical alignment property

--- js/themeBlockView.js
+++ js/themeBlockView.js
@@ -6,7 +6,26 @@ export default class ThemeBlockView extends ThemeView {
     return this.model.get('_isDividerBlock') ? 'is-divider-block' : '';
   }
 
-  setCustomStyles() {}
+  setCustomStyles() {
+    this.setComponentVerticalAlignment();
+  }
+
+  setComponentVerticalAlignment() {
+    const componentVerticalAlignment = this.model.get('_componentVerticalAlignment');
+    if (!componentVerticalAlignment) return;
+
+    switch (componentVerticalAlignment) {
+      case 'center':
+        this.$el.addClass('align-vert-center');
+        break;
+      case 'bottom':
+        this.$el.toggleClass('align-vert-bottom');
+        break;
+      default:
+        this.$el.toggleClass('align-vert-top');
+        break;
+    }
+  }
 
   onRemove() {}
 
