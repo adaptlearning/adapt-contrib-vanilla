5a9b2a54a32d0c59f86abe77f1bf1a89adab052c
Guy Willis <guy.willis@kineo.com>
2022-11-04 16:18:50 +0000

    Optimised class application

--- js/themeBlockView.js
+++ js/themeBlockView.js
@@ -13,18 +13,7 @@ export default class ThemeBlockView extends ThemeView {
   setComponentVerticalAlignment() {
     const componentVerticalAlignment = this.model.get('_componentVerticalAlignment');
     if (!componentVerticalAlignment) return;
-
-    switch (componentVerticalAlignment) {
-      case 'center':
-        this.$el.addClass('align-vert-center');
-        break;
-      case 'bottom':
-        this.$el.toggleClass('align-vert-bottom');
-        break;
-      default:
-        this.$el.toggleClass('align-vert-top');
-        break;
-    }
+    this.$el.addClass(`align-vert-${componentVerticalAlignment}`);
   }
 
   onRemove() {}
